c965294c4329ba0130d279e6309e87dd6d21847e
aleksandrbelaev <alexanderbelyaev1990@gmail.com>
2022-07-10 18:42:02 +0200

    tпреписал на автоматическое создание id  в бд

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -20,10 +20,15 @@ public class App
 
         try{
             session.beginTransaction();
+            Person person1 = new Person("Ivan", 30);
+            Person person2 = new Person("Tom", 20);
+            Person person3 = new Person("Petr", 50);
 
-            Person person = session.get(Person.class, 1);
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+
+            session.getTransaction().commit();
         }
         finally {
             sessionFactory.close();
